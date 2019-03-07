f2d76c3c202ae27a57c582ed580e219bc3fd3348
economistgame <agustin.perez01@goumh.umh.es>
2019-03-07 18:01:43 +0100

    New tests

--- tests/testthat/test-Clustering.R
+++ tests/testthat/test-Clustering.R
@@ -8,3 +8,11 @@ test_that("Test Clustering with EGATUR dataset.", {
   expect_equal(class(modelFit[[2]]),"kmeans" )
 
 })
+
+test_that("Test Clustering with EGATUR dataset and n==Character.", {
+modelFit2 <- Clustering(data=EGATUR[,c("A13","gastototal")],n="moto")
+expect_equal(class(modelFit2), "MLA")
+expect_equal(modelFit2[[1]], "Clustering")
+expect_equal(class(modelFit2[[2]]),"kmeans" )
+
+})
